1fb264178735ee04fe088f3cabdc2c20cbeb8947
Hanwook Ryu <ryu@hanwook.me>
2022-12-27 20:39:23 +0900

    Feat: 최초 데이터 추가

---
+++ src/main/kotlin/me/hanwook/graphql/config/InitDataConfig.kt
@@ -0,0 +1,36 @@
+package me.hanwook.graphql.config
+
+import me.hanwook.graphql.domain.Gender
+import me.hanwook.graphql.domain.User
+import me.hanwook.graphql.domain.UserFavorite
+import me.hanwook.graphql.domain.UserFavoriteType
+import me.hanwook.graphql.repository.UserRepository
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import javax.annotation.PostConstruct
+import kotlin.random.Random
+
+@Component
+class InitDataConfig(
+    val userRepository: UserRepository
+) {
+
+    @PostConstruct
+    @Transactional
+    fun init() {
+        val names = arrayListOf("Woo", "Wook", "Ryu")
+        val random = Random.Default
+        val genders = Gender.values()
+        val favoriteTypes = UserFavoriteType.values()
+
+        for(i in 1..30) {
+            val user = User(name = names.random(), gender = genders[random.nextInt(genders.size)])
+
+            for(j in 0 .. random.nextInt(favoriteTypes.size)) {
+                UserFavorite(user = user, favoriteType = favoriteTypes[j])
+            }
+
+            userRepository.save(user)
+        }
+    }
+}
